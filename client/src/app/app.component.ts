dd9f19fbe869997f817c07ecd60aca52b7fd1f77
Reno Perri <perrir@upmc.edu>
2020-12-02 20:40:33 -0500

    End of Section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,11 +13,10 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
 
-  constructor (private http: HttpClient, private accountService: AccountService){
+  constructor (private accountService: AccountService){
 
   }
   ngOnInit(): void {
-   this.getUsers();
    this.setCurrentUser();
   }
 
@@ -26,12 +25,5 @@ export class AppComponent implements OnInit {
     this.accountService.setCurrentUser(user);
 
   }
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(response =>{
-      this.users = response;
-    }, error => {
-      console.log(error);
-    })
 
-  }
 }
