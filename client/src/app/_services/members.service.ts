7ca38521ffe51b26e20356269a7d536d3683ffd3
Reno Perri <perrir@upmc.edu>
2020-12-06 22:44:38 -0500

    update

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { htmlAstToRender3Ast } from '@angular/compiler/src/render3/r3_template_transform';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Member } from '../_models/member';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user') || '{}')?.token
+  })
+
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getMembers(){
+    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+  }
+
+  getMember(username: string){
+    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions);
+  }
+}
